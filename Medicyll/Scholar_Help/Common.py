876e4bb6dac349e6bc110f96baf54e051b421c5f
teamzero-aaq <abdulquadir828@hmail.com>
2020-08-09 14:37:55 +0530

    Digital Sign

---
+++ Medicyll/Scholar_Help/Common.py
@@ -0,0 +1,16 @@
+isLogin = False
+currentUser = None
+isTrustLogin = False
+trustkey = None
+trustVal = None
+forgotpassotp = None
+forgotpassotptime = None
+userphone = None
+encyptionkey = b'5mgdtkgjMcGj2hBGXAIqA7fcaEeS0_LAT4N8wRNF0ZA='
+isAdminLogin = False
+admin = None
+adharotp = None
+adharotptime = None
+tempuser = None
+schemedetails=None
+# comment
